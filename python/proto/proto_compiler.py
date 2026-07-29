a2227eae22ec7c9f0c5c6b810df7ec3c3ff3f0b4
Kevin Geiszler <kevin.j.geiszler@gmail.com>
2026-07-29 14:49:47 -0400

    Add log message to proto_compiler.py regarding what proto file is compiled

--- python/proto/proto_compiler.py
+++ python/proto/proto_compiler.py
@@ -5,6 +5,9 @@ This script removes any existing 'generated' directory and creates a new one eve
 """
 import os
 from grpc_tools import protoc
+from python.src.logger_config import get_logger
+
+logger = get_logger(__name__)
 
 
 if __name__ == '__main__':
@@ -17,6 +20,7 @@ if __name__ == '__main__':
 
     proto_files = [file for file in os.listdir(proto_dir) if file.endswith('.proto')]
     for file in proto_files:
+        logger.info(f"Compiling {file} and sending output to {generated_dir}")
         protoc.main((
             '',
             f'-I{proto_dir}',
