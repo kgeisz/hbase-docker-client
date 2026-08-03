61a71c9bc76c103eded682949038a6b863dcf01b
Kevin Geiszler <kevin.j.geiszler@gmail.com>
2026-08-03 12:51:47 -0400

    Add python/scripts/python_console_env_setup.py

---
+++ python/scripts/python_console_env_setup.py
@@ -0,0 +1,22 @@
+"""
+Use this script to quickly set up HBaseDockerClient objects in your Python console.
+
+To set up the environment, start the Python console and run (path may need to be adjusted):
+
+exec(open("python/scripts/python_console_env_setup.py").read())
+"""
+from python.src.environment_loader import get_env
+from python.src.utils import load_env_and_set_up_clients
+
+cluster1, cluster2 = load_env_and_set_up_clients()
+
+hbase_image = get_env("HBASE_IMAGE")
+hbase_container_name = get_env("HBASE_CONTAINER_NAME")
+hbase_conf_dir = get_env("HBASE_CONF_DIR")
+data_store_root = get_env("HBASE_DATA_STORE_ROOT")
+active_cluster_port = get_env("ACTIVE_CLUSTER_PORT")
+replica_cluster_port = get_env("REPLICA_CLUSTER_PORT")
+active_cluster_conf_dir = get_env("ACTIVE_CLUSTER_CONF_DIR")
+replica_cluster_conf_dir = get_env("REPLICA_CLUSTER_CONF_DIR")
+docker_compose_file = get_env("DOCKER_COMPOSE_FILE")
+log_level = get_env("LOG_LEVEL")
